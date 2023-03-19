7712ee7abcde0f7dbf914bbca53b68b1dc66f4c9
nahnahAJ <nahnahahjuwah@gmail.com>
2023-03-19 04:58:09 +0000

    Add the navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React from 'react';
+import Navbar from './Navbar';
+import './input.css';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="bg-gray-100 h-screen">
+      <Navbar />
+      <div className="container mx-auto px-4 mt-8">
+        <h1 className="text-3xl font-bold mb-4">Welcome to my portfolio website</h1>
+        <p className="mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet magna felis, at iaculis sapien ultricies eu.</p>
+        <a href="#" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Learn more</a>
+      </div>
     </div>
   );
-};
+}
 
 export default App;
