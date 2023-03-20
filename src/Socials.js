de4df334e37f810a65daa61b375ed3371b899692
nahnahAJ <nahnahahjuwah@gmail.com>
2023-03-20 15:09:35 +0000

    Remove social media

--- src/Socials.js
+++ src/Socials.js
@@ -164,27 +164,6 @@ const Socials = () => (
         />
       </svg>
     </a>
-
-    {/* Snapchat */}
-    <a
-      href="https://www.snapchat.com/add/nahnah_ahjuwah"
-      target="_blank"
-      rel="noreferrer"
-      type="button"
-      data-te-ripple-init
-      data-te-ripple-color="light"
-      className="mb-2 inline-block rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..."
-      style={{ backgroundColor: '#f8cc1b' }}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-4 w-4"
-        fill="currentColor"
-        viewBox="0 0 24 24"
-      >
-        <path d="M5.829 4.533c-.6 1.344-.363 3.752-.267 5.436-.648.359-1.48-.271-1.951-.271-.49 0-1.075.322-1.167.802-.066.346.089.85 1.201 1.289.43.17 1.453.37 1.69.928.333.784-1.71 4.403-4.918 4.931-.251.041-.43.265-.416.519.056.975 2.242 1.357 3.211 1.507.099.134.179.7.306 1.131.057.193.204.424.582.424.493 0 1.312-.38 2.738-.144 1.398.233 2.712 2.215 5.235 2.215 2.345 0 3.744-1.991 5.09-2.215.779-.129 1.448-.088 2.196.058.515.101.977.157 1.124-.349.129-.437.208-.992.305-1.123.96-.149 3.156-.53 3.211-1.505.014-.254-.165-.477-.416-.519-3.154-.52-5.259-4.128-4.918-4.931.236-.557 1.252-.755 1.69-.928.814-.321 1.222-.716 1.213-1.173-.011-.585-.715-.934-1.233-.934-.527 0-1.284.624-1.897.286.096-1.698.332-4.095-.267-5.438-1.135-2.543-3.66-3.829-6.184-3.829-2.508 0-5.014 1.268-6.158 3.833z" />
-      </svg>
-    </a>
   </div>
 );
 
